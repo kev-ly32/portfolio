081357e8d5443970f7fd5eb1e8389fba0cd2cf1c
Kevin Ly <kev_ly32@hotmail.com>
2021-02-04 21:47:11 -0500

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,32 @@
+function App() {
+  return (
+    <div className="App">
+      <h1>Kevin Ly</h1>
+      <a
+        href="https://flow-employee-database.herokuapp.com/"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Employee Database
+      </a>
+      <br />
+      <a
+        href="https://aptitudebank.herokuapp.com/"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Apitude Bank
+      </a>
+      <br />
+      <a
+        href="https://have-it-noted.herokuapp.com/"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Noted
+      </a>
+    </div>
+  );
+}
+
+export default App;
