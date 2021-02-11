b32f8ce6cf5892d350bd7d49373684a78b78b9fb
Kevin Ly <kev_ly32@hotmail.com>
2021-02-10 21:22:16 -0500

    updated projects section

--- client/src/components/Projects.js
+++ client/src/components/Projects.js
@@ -1,24 +1,63 @@
 import React from "react";
-import { Grid } from "@material-ui/core";
+import { Grid, Typography, Card, CardMedia } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import background from "../public/Background.jpg";
 
 const useStyles = makeStyles({
   container: {
-    height: "100%",
-    backgroundImage: `url(${background})`,
-    backgroundSize: "cover",
-    backgroundAttachment: "fixed",
-    backgroundPosition: "center",
+    height: "80%",
+    backgroundColor: "#F5F5F5",
+    padding: "5% 0",
+    // backgroundImage: `url(${background})`,
+    // backgroundSize: "cover",
+    // backgroundAttachment: "fixed",
+    // backgroundPosition: "center",
+  },
+  header: {
+    height: "10%",
+    textAlign: "center",
+  },
+  projectsContainer: {
+    width: "90%",
+    margin: "0 auto",
+  },
+  project: {
+    height: " 300px",
   },
 });
 
 function Projects(props) {
   const classes = useStyles();
   return (
-    <Grid container className={classes.container}>
-      <Grid item>
-        <h1>Projects!</h1>
+    <Grid justify="center" container className={classes.container}>
+      <Grid item xs={12} className={classes.header}>
+        <Typography variant="h4">Projects!</Typography>
+      </Grid>
+      <Grid spacing={4} container className={classes.projectsContainer}>
+        <Grid item md={4}>
+          <Card>
+            <CardMedia
+              className={classes.project}
+              image="https://res.cloudinary.com/de5gzocha/image/upload/v1613008132/Portfolio/employee-database_tlmo9d.png"
+            />
+          </Card>
+        </Grid>
+        <Grid item md={4}>
+          <Card>
+            <CardMedia
+              className={classes.project}
+              image="https://res.cloudinary.com/de5gzocha/image/upload/v1613008132/Portfolio/aptitude-bank_m9khsm.png"
+            />
+          </Card>
+        </Grid>
+        <Grid item md={4}>
+          <Card>
+            <CardMedia
+              className={classes.project}
+              image="https://res.cloudinary.com/de5gzocha/image/upload/v1613008132/Portfolio/noted_wwosxj.png"
+            />
+          </Card>
+        </Grid>
       </Grid>
     </Grid>
   );
