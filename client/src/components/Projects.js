befdf4eaf4b319936802ce8b145b8f72d49cebef
Kevin Ly <kev_ly32@hotmail.com>
2021-02-11 21:35:34 -0500

    added project card hover styling

--- client/src/components/Projects.js
+++ client/src/components/Projects.js
@@ -1,7 +1,14 @@
 import React from "react";
 import { Grid, Typography, Card, CardMedia } from "@material-ui/core";
+import VisibilityIcon from "@material-ui/icons/Visibility";
 import { makeStyles } from "@material-ui/core/styles";
 import background from "../public/Background.jpg";
+
+const projects = [
+  "https://res.cloudinary.com/de5gzocha/image/upload/v1613008132/Portfolio/employee-database_tlmo9d.png",
+  "https://res.cloudinary.com/de5gzocha/image/upload/v1613008132/Portfolio/aptitude-bank_m9khsm.png",
+  "https://res.cloudinary.com/de5gzocha/image/upload/v1613008132/Portfolio/noted_wwosxj.png",
+];
 
 const useStyles = makeStyles({
   container: {
@@ -22,7 +29,31 @@ const useStyles = makeStyles({
     margin: "0 auto",
   },
   project: {
+    zIndex: "1",
     height: " 300px",
+    position: "relative",
+  },
+  layer: {
+    height: "300px",
+    zIndex: "2",
+    position: "relative",
+    top: "-300px",
+    color: "white",
+    textAlign: "center",
+    alignItems: "center",
+    justifyContent: "center",
+    "& .layerContent": { display: "none" },
+    "&:hover": {
+      backgroundColor: "rgba(0,0,0,0.75)",
+      cursor: "pointer",
+    },
+    "&:hover .layerContent": { display: "flex" },
+  },
+  layerContent: {
+    opacity: "0",
+    "&:hover": {
+      opacity: "1",
+    },
   },
 });
 
@@ -34,30 +65,17 @@ function Projects(props) {
         <Typography variant="h4">Projects!</Typography>
       </Grid>
       <Grid spacing={4} container className={classes.projectsContainer}>
-        <Grid item md={4}>
-          <Card>
-            <CardMedia
-              className={classes.project}
-              image="https://res.cloudinary.com/de5gzocha/image/upload/v1613008132/Portfolio/employee-database_tlmo9d.png"
-            />
-          </Card>
-        </Grid>
-        <Grid item md={4}>
-          <Card>
-            <CardMedia
-              className={classes.project}
-              image="https://res.cloudinary.com/de5gzocha/image/upload/v1613008132/Portfolio/aptitude-bank_m9khsm.png"
-            />
-          </Card>
-        </Grid>
-        <Grid item md={4}>
-          <Card>
-            <CardMedia
-              className={classes.project}
-              image="https://res.cloudinary.com/de5gzocha/image/upload/v1613008132/Portfolio/noted_wwosxj.png"
-            />
-          </Card>
-        </Grid>
+        {projects.map((project) => (
+          <Grid item xs={12} sm={6} md={4} style={{ height: "350px" }}>
+            <Card square>
+              <CardMedia className={classes.project} image={project} />
+            </Card>
+            <Grid container direction="column" className={classes.layer}>
+              <VisibilityIcon className="layerContent " />
+              <Typography className="layerContent">View Project</Typography>
+            </Grid>
+          </Grid>
+        ))}
       </Grid>
     </Grid>
   );
