6dd6c2487f67433769a6d2b20ee4943c9b1fc900
Kevin Ly <kev_ly32@hotmail.com>
2021-02-13 15:49:10 -0500

    updated project details section and updated projects array to be objects

--- client/src/components/Projects.js
+++ client/src/components/Projects.js
@@ -2,23 +2,61 @@ import React, { useState } from "react";
 import { Grid, Typography, Card, CardMedia } from "@material-ui/core";
 import VisibilityIcon from "@material-ui/icons/Visibility";
 import { makeStyles } from "@material-ui/core/styles";
-import background from "../public/Background.jpg";
 
 const projects = [
-  "https://res.cloudinary.com/de5gzocha/image/upload/v1613008132/Portfolio/employee-database_tlmo9d.png",
-  "https://res.cloudinary.com/de5gzocha/image/upload/v1613008132/Portfolio/aptitude-bank_m9khsm.png",
-  "https://res.cloudinary.com/de5gzocha/image/upload/v1613008132/Portfolio/noted_wwosxj.png",
+  {
+    name: "Flow Database",
+    img:
+      "https://res.cloudinary.com/de5gzocha/image/upload/v1613008132/Portfolio/employee-database_tlmo9d.png",
+    description:
+      "Employee database application that allows users to update an individual database to manage employee data for a particular company.",
+    stack: [
+      "https://res.cloudinary.com/de5gzocha/image/upload/v1612841339/Portfolio/html_otphxd.png",
+      "https://res.cloudinary.com/de5gzocha/image/upload/v1612841339/Portfolio/css_un8uqf.png",
+      "https://res.cloudinary.com/de5gzocha/image/upload/v1612841339/Portfolio/javascript_cpxt37.png",
+      "https://res.cloudinary.com/de5gzocha/image/upload/v1612841339/Portfolio/react_xdhtjl.png",
+      "https://res.cloudinary.com/de5gzocha/image/upload/v1612841339/Portfolio/mongodb_s43pgt.png",
+      "https://res.cloudinary.com/de5gzocha/image/upload/v1612922901/Portfolio/nodejs_stx37t.png",
+    ],
+  },
+  {
+    name: "Aptitude Bank",
+    img:
+      "https://res.cloudinary.com/de5gzocha/image/upload/v1613008132/Portfolio/aptitude-bank_m9khsm.png",
+    description:
+      "Banking app featuring full user authentication and real time balance updates. Aptitude Bank also features the ability to send and receive money to other users and updates account transaction history for all balance updates.",
+    stack: [
+      "https://res.cloudinary.com/de5gzocha/image/upload/v1612841339/Portfolio/html_otphxd.png",
+      "https://res.cloudinary.com/de5gzocha/image/upload/v1612841339/Portfolio/css_un8uqf.png",
+      "https://res.cloudinary.com/de5gzocha/image/upload/v1612841339/Portfolio/javascript_cpxt37.png",
+      "https://res.cloudinary.com/de5gzocha/image/upload/v1612841339/Portfolio/react_xdhtjl.png",
+      "https://res.cloudinary.com/de5gzocha/image/upload/v1612841339/Portfolio/redux_r4cguj.png",
+      "https://res.cloudinary.com/de5gzocha/image/upload/v1612841339/Portfolio/mongodb_s43pgt.png",
+      "https://res.cloudinary.com/de5gzocha/image/upload/v1612922901/Portfolio/nodejs_stx37t.png",
+    ],
+  },
+  {
+    name: "Noted",
+    img:
+      "https://res.cloudinary.com/de5gzocha/image/upload/v1613008132/Portfolio/noted_wwosxj.png",
+    description:
+      "Personalized note taking app with full user authentication. App features the ability to not only add, update, and remove notes, but also allows users to move each note within the dashboard whereever they please, organizing them in a way that suits their needs. Note position is updated in real time and saves to a Postgresql database.",
+    stack: [
+      "https://res.cloudinary.com/de5gzocha/image/upload/v1612841339/Portfolio/html_otphxd.png",
+      "https://res.cloudinary.com/de5gzocha/image/upload/v1612841339/Portfolio/css_un8uqf.png",
+      "https://res.cloudinary.com/de5gzocha/image/upload/v1612841339/Portfolio/javascript_cpxt37.png",
+      "https://res.cloudinary.com/de5gzocha/image/upload/v1612841339/Portfolio/react_xdhtjl.png",
+      "https://res.cloudinary.com/de5gzocha/image/upload/v1612841339/Portfolio/mui_f1zwfn.png",
+      "https://res.cloudinary.com/de5gzocha/image/upload/v1612841339/Portfolio/postgresql_truxlj.png",
+      "https://res.cloudinary.com/de5gzocha/image/upload/v1612922901/Portfolio/nodejs_stx37t.png",
+    ],
+  },
 ];
 
 const useStyles = makeStyles({
   container: {
-    height: "80%",
     backgroundColor: "#F5F5F5",
     padding: "5% 0",
-    // backgroundImage: `url(${background})`,
-    // backgroundSize: "cover",
-    // backgroundAttachment: "fixed",
-    // backgroundPosition: "center",
   },
   header: {
     height: "10%",
@@ -26,7 +64,7 @@ const useStyles = makeStyles({
   },
   projectsContainer: {
     width: "90%",
-    margin: "0 auto",
+    margin: "0 auto 5% auto",
   },
   project: {
     zIndex: "1",
@@ -49,6 +87,9 @@ const useStyles = makeStyles({
     },
     "&:hover .layerContent": { display: "flex" },
   },
+  description: {
+    paddingLeft: "5%",
+  },
 });
 
 function Projects(props) {
@@ -68,7 +109,7 @@ function Projects(props) {
         {projects.map((project, i) => (
           <Grid key={i} item xs={12} sm={6} md={4} style={{ height: "350px" }}>
             <Card square>
-              <CardMedia className={classes.project} image={project} />
+              <CardMedia className={classes.project} image={project.img} />
             </Card>
             <Grid
               container
@@ -92,9 +133,46 @@ function Projects(props) {
           </Grid>
         ))}
       </Grid>
-      <Grid container>
-        <h1>Project details</h1>
-        <img src={project} alt={project} />
+      <Grid container justify="center">
+        <Grid container style={{ width: "90%" }}>
+          {project !== "" ? (
+            <>
+              <Grid item xs={12}>
+                <Typography gutterBottom variant="h4">
+                  {project.name}
+                </Typography>
+              </Grid>
+              <Grid item md={7}>
+                <img
+                  src={project.img}
+                  alt={project.img}
+                  style={{
+                    maxHeight: "100%",
+                    maxWidth: "100%",
+                    width: "auto",
+                    height: "auto",
+                  }}
+                />
+              </Grid>
+              <Grid item md={5} className={classes.description}>
+                <Typography color="secondary" variant="h6">
+                  Description:
+                </Typography>
+                <Typography paragraph variant="p">
+                  {project.description}
+                </Typography>
+                <Typography color="secondary" variant="h6">
+                  Tech stack:
+                </Typography>
+                <Typography variant="p">
+                  {project.stack.map((logo, i) => (
+                    <img key={i} src={logo} alt={logo} />
+                  ))}
+                </Typography>
+              </Grid>
+            </>
+          ) : null}
+        </Grid>
       </Grid>
     </Grid>
   );
