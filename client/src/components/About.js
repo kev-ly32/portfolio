6f21b3d338c3317e72a59e13f8d10d977757c654
Kevin Ly <kev_ly32@hotmail.com>
2021-02-22 15:53:10 -0500

    updated about section styling

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -1,61 +1,8 @@
 import React from "react";
-import { Button, IconButton, Grid } from "@material-ui/core";
+import { Button, IconButton, Grid, Typography } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
-
-const logos = [
-  {
-    link:
-      "https://res.cloudinary.com/de5gzocha/image/upload/v1612841339/Portfolio/html_otphxd.png",
-    name: "HTML5",
-  },
-  {
-    link:
-      "https://res.cloudinary.com/de5gzocha/image/upload/v1612841339/Portfolio/css_un8uqf.png",
-    name: "CSS3",
-  },
-  {
-    link:
-      "https://res.cloudinary.com/de5gzocha/image/upload/v1612841339/Portfolio/javascript_cpxt37.png",
-    name: "Javascript",
-  },
-  {
-    link:
-      "https://res.cloudinary.com/de5gzocha/image/upload/v1612841339/Portfolio/react_xdhtjl.png",
-    name: "React",
-  },
-  {
-    link:
-      "https://res.cloudinary.com/de5gzocha/image/upload/v1612841339/Portfolio/redux_r4cguj.png",
-    name: "Redux",
-  },
-  {
-    link:
-      "https://res.cloudinary.com/de5gzocha/image/upload/v1612841260/Portfolio/bootstrap_mkoxvy.png",
-    name: "Bootstrap",
-  },
-  {
-    link:
-      "https://res.cloudinary.com/de5gzocha/image/upload/v1612841339/Portfolio/mui_f1zwfn.png",
-    name: "Material-UI",
-  },
-  {
-    link:
-      "https://res.cloudinary.com/de5gzocha/image/upload/v1612841339/Portfolio/mongodb_s43pgt.png",
-    name: "MongoDB",
-  },
-  {
-    link:
-      "https://res.cloudinary.com/de5gzocha/image/upload/v1612841339/Portfolio/postgresql_truxlj.png",
-    name: "PostgreSQL",
-  },
-  {
-    link:
-      "https://res.cloudinary.com/de5gzocha/image/upload/v1612922901/Portfolio/nodejs_stx37t.png",
-    name: "Node.js",
-  },
-];
 
 const useStyles = makeStyles((theme) => ({
   container: {
@@ -64,17 +11,14 @@ const useStyles = makeStyles((theme) => ({
     backgroundColor: "#F5F5F5",
   },
   aboutContainer: {
-    height: "60%",
+    height: "50%",
     width: "60%",
-    border: "2px dotted black",
   },
-  aboutBody: {
-    textAlign: "center",
-  },
+
   aboutImage: {
-    height: "40%",
+    height: "200px",
     display: "flex",
-    justifyContent: "center",
+    // justifyContent: "center",
   },
   logo: {
     background: "none",
@@ -105,42 +49,59 @@ function About({ aboutRef, scroll }) {
     >
       <Grid
         container
-        justify="center"
-        alignItems="center"
+        // justify="center"
+        // alignItems="center"
         className={`${classes.aboutContainer} fade-in`}
       >
-        <Grid item md={4} className={classes.aboutImage}>
+        <Grid item xs={12}>
+          <Typography variant="h3">About Me</Typography>
+        </Grid>
+
+        <Grid item xs={12} lg={4} className={classes.aboutImage}>
           <img
             src="https://res.cloudinary.com/de5gzocha/image/upload/v1613622911/Portfolio/KevinLy_glphuk.jpg"
             alt="Kevin Ly"
-            style={{ height: "100%", borderRadius: "150px" }}
+            // style={{ height: "100%", borderRadius: "150px" }}
           />
         </Grid>
-        <Grid item md={8} className={classes.aboutBody}>
-          <h2 style={{ marginBottom: "3%" }}>About Me</h2>
+        <Grid item xs={12} lg={8}>
           <p style={{ marginBottom: "3%" }}>
-            Nice to meet you! My name's Kevin and I'm a web developer <br />
-            with a passion for{" "}
+            Nice to meet you! My name's Kevin and I'm a web developer with a
+            passion for{" "}
             <span style={{ color: "#f57c00" }}>full stack development</span>. My
-            goal is to provide
-            <br /> a meaningful impact to the projects I work on to create
-            <span style={{ color: "#f57c00" }}> beautiful</span> <br /> and
+            goal is to provide a meaningful impact to the projects I work on to
+            create
+            <span style={{ color: "#f57c00" }}> beautiful</span> and
             <span style={{ color: "#f57c00" }}> functional</span> web
             applications.
           </p>
-          <p style={{ marginBottom: "3%" }}>
-            To do that, I have developed a skillset with the following
-            technologies:
-          </p>
-          <Grid container justify="center">
-            {logos.map((logo, i) => (
-              <div key={i} style={{ maxWidth: "50px", height: "70px" }}>
-                <img className={classes.logo} src={logo.link} alt="logo" />
-                <p className="logoName" style={{ fontSize: "13px" }}>
-                  {logo.name}
-                </p>
-              </div>
-            ))}
+          <hr style={{ marginBottom: "3%" }} />
+          <Grid container>
+            <Grid item xs={12} sm={6}>
+              <Typography variant="body1">
+                Name: <span className="aboutInfo">Kevin Ly</span>
+              </Typography>
+              <Typography variant="body1">
+                From: <span className="aboutInfo">Toronto, ON</span>
+              </Typography>
+            </Grid>
+            <Grid item xs={12} md={6}>
+              <Typography variant="body1">
+                Email: <span className="aboutInfo"> kevin.ly153@gmail.com</span>
+              </Typography>
+            </Grid>
+            <Grid item xs={12} style={{ marginTop: "5%" }}>
+              <Button color="secondary" variant="outlined">
+                View Resume
+              </Button>
+              <Button
+                color="primary"
+                variant="outlined"
+                style={{ marginLeft: "20px" }}
+              >
+                Projects
+              </Button>
+            </Grid>
           </Grid>
           {/* <br />
           <Button variant="outlined" style={{ marginTop: "5%" }}>
