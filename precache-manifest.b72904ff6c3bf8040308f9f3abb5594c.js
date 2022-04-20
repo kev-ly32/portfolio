2325ad3468431b117ab7fa6aec1de1d4ce3e00fb
Kevin Ly <kev_ly32@hotmail.com>
2022-04-20 08:42:12 -0400

    Updates

---
+++ precache-manifest.b72904ff6c3bf8040308f9f3abb5594c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "153b3665d853486c49fc",
+    "url": "/portfolio/static/css/main.2e49dc3f.chunk.css"
+  },
+  {
+    "revision": "153b3665d853486c49fc",
+    "url": "/portfolio/static/js/main.153b3665.chunk.js"
+  },
+  {
+    "revision": "94f20bb72f660b09551c",
+    "url": "/portfolio/static/js/1.94f20bb7.chunk.js"
+  },
+  {
+    "revision": "86da478ff5c78a86fa3d",
+    "url": "/portfolio/static/js/2.86da478f.chunk.js"
+  },
+  {
+    "revision": "c1d1ff2a818147ef4b79",
+    "url": "/portfolio/static/js/runtime~main.c1d1ff2a.js"
+  },
+  {
+    "revision": "512dc4a163c9a6d605b367f45dcaf9c3",
+    "url": "/portfolio/static/media/Background.512dc4a1.jpg"
+  },
+  {
+    "revision": "6f62580b4dbafeb1e86e1e7bd32e74cc",
+    "url": "/portfolio/index.html"
+  }
+];
